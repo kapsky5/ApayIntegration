bb5f766cef1730c4d5a3edf49f2c6a92674074b7
Karan Purswani <karan12598@gmail.com>
2024-12-15 09:34:34 +0530

    Adding APay Java SDK and adding Ping Controller.

--- src/main/java/com/kapsky/apay/integ/Application.java
+++ src/main/java/com/kapsky/apay/integ/Application.java
@@ -1,22 +1,13 @@
 package com.kapsky.apay.integ;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@GetMapping("/home")
-	public String renderHome(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
 }
