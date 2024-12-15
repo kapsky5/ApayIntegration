42d507a33ae39697d81673af218369ea71549aeb
Karan Purswani <karan12598@gmail.com>
2024-12-15 22:25:26 +0530

    Adding log4j2

--- src/main/java/com/kapsky/apay/integ/controller/CheckoutSessionController.java
+++ src/main/java/com/kapsky/apay/integ/controller/CheckoutSessionController.java
@@ -2,6 +2,7 @@ package com.kapsky.apay.integ.controller;
 
 import com.kapsky.apay.integ.model.CreateCheckoutSessionRequest;
 import com.kapsky.apay.integ.model.CreateCheckoutSessionResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/checkoutSession")
+@Log4j2
 public class CheckoutSessionController {
 
     @PostMapping
-    public ResponseEntity<CreateCheckoutSessionResponse> createCheckoutSession(final CreateCheckoutSessionRequest createCheckoutSessionRequest) {
-
+    public ResponseEntity<CreateCheckoutSessionResponse> createCheckoutSession(
+            final CreateCheckoutSessionRequest createCheckoutSessionRequest) {
+        log.info("CreateCheckoutSession Request: {}", createCheckoutSessionRequest);
         return ResponseEntity.ok(CreateCheckoutSessionResponse.builder()
                         .checkoutSessionId("123")
                         .build());
